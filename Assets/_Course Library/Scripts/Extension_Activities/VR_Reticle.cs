672c059a5f5bb7e3a6f684607891b7dc973676eb
Evan Williams <1evan.williams1@gmail.com>
2023-12-09 16:31:28 -0500

    FInished chapter 1.2 VR Locomotion

---
+++ Assets/_Course Library/Scripts/Extension_Activities/VR_Reticle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_Reticle : MonoBehaviour
+{
+    private Transform _reticleTransform;
+    [SerializeField] private float _RotateSpeed;
+    [SerializeField] private float _ScaleSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _reticleTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Scale();
+    }
+
+    void Scale()
+    {
+        float scaleAmount = Mathf.Abs(Mathf.Sin(Time.time * _ScaleSpeed)) + 1;
+        Vector3 scale =
+            new Vector3(scaleAmount, scaleAmount, scaleAmount);
+
+        _reticleTransform.localScale = scale;
+    }
+
+    void Rotate()
+    {
+        float angle = Time.time * _RotateSpeed;
+
+        Quaternion rotation =
+           Quaternion.Euler(_reticleTransform.localEulerAngles.x, angle, _reticleTransform.localEulerAngles.z);
+
+        _reticleTransform.localRotation = rotation;
+    }
+}
